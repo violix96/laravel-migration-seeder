6c4579caeadb77374bcc455935d154f807325629
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-07-09 17:07:34 +0200

    [UPD] creating the table with the command php artisan migrate

--- database/migrations/2024_07_09_133417_create_trains_table.php
+++ database/migrations/2024_07_09_133417_create_trains_table.php
@@ -14,6 +14,15 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('azienda');
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno')->unique();
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
         });
     }
 
